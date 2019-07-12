5d87e4c52e79b3576fb992243e2eb465503879d8
Davie <dhdavidnguyen@gmail.com>
2019-07-11 21:31:15 -0700

    Update

--- stocks.py
+++ stocks.py
@@ -5,10 +5,11 @@ import ruptures as rpt
 msft = yf.Ticker("SPY")
 
 data = yf.download('spy', start= '2019-01-01', end='2019-07-11')
-data = data['Close']
+data = data.Close.values
 
 algo = rpt.Pelt(model='rbf').fit(data)
 result = algo.predict(pen=10)
 
 rpt.display(data, result=result)
 plt.show()
+
